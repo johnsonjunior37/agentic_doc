eecb3a3854cf4100d1072dac009cff39f88c6aba
johnsonjunior37 <johnsonjunior37@gmail.com>
2025-08-06 20:32:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from agentic_doc.parse import parse_documents
 from agentic_doc.utils import viz_parsed_document
+import os
 
+# 將你的 API 金鑰存在環境變數中
+os.environ["VISION_AGENT_API_KEY"] = "YTluYnhqNjBscW5xcTZsZjZhYzVzOjF1aWdBcEJMUGNWQ1pYdEtPYXdoRVhJWUw4YnBlV0xN"
 # 假設有 input.pdf
 pdf_path = "test1.pdf"
 results = parse_documents([pdf_path])
